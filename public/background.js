3709dd25dd7b7eaa0b2334fb8dee25f6d64c4d7e
Josh <74725025+JoshuaSloan9@users.noreply.github.com>
2024-10-27 20:42:39 +0000

    commit

--- public/background.js
+++ public/background.js
@@ -1,5 +1,5 @@
 /*global chrome*/
 
 chrome.action.onClicked.addListener(function(tab) {
-    chrome.tabs.create({url: 'https://your-react-app-url.com'});
+    chrome.tabs.create({url: 'https://giu484.github.io/ubiquitous-octo-waddle'});
 });
